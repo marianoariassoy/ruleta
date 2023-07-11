bdff9e287d93b009b8adda41600f88b620c3d615
marianoariassoy <hola@marianoarias.soy>
2023-07-11 20:00:21 -0300

    add x

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,61 +2,61 @@ export const premios = [
   {
     id: 1,
     name: "Möoi",
-    stock: 2,
+    stock: 3,
   },
   {
     id: 2,
     name: "Tostado",
-    stock: 3,
+    stock: 2,
   },
   {
     id: 3,
     name: "Pizzeria Popular",
-    stock: 2,
+    stock: 3,
   },
   {
     id: 4,
     name: "Brioche Dorée",
-    stock: 3,
+    stock: 2,
   },
   {
     id: 5,
     name: "Green & co",
-    stock: 2,
+    stock: 3,
   },
   {
     id: 6,
     name: "Z Food",
-    stock: 3,
+    stock: 2,
   },
   {
     id: 7,
     name: "Möoi",
-    stock: 3,
+    stock: 2,
   },
   {
     id: 8,
     name: "Tostado",
-    stock: 2,
+    stock: 3,
   },
   {
     id: 9,
     name: "Pizzeria Popular",
-    stock: 3,
+    stock: 2,
   },
   {
     id: 10,
     name: "Brioche Dorée",
-    stock: 2,
+    stock: 3,
   },
   {
     id: 11,
     name: "Green & co",
-    stock: 3,
+    stock: 2,
   },
   {
     id: 12,
     name: "Z Food",
-    stock: 2,
+    stock: 3,
   },
 ];
