fdc85737d9147fb66c88c19daaa5221c4f7d6051
marianoariassoy <hola@marianoarias.soy>
2023-07-11 18:49:22 -0300

    add menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { premios } from "../data/data";
+// import { premios } from "../data/data";
 
 type Props = {
   setMenu: Function;
@@ -6,12 +6,12 @@ type Props = {
   stock: Array<any>;
 };
 
-const Menu = ({ stock, setStock, setMenu }: Props) => {
-  const resetStock = () => {
-    setStock(premios);
-    console.log(stock);
-    console.log(premios);
-  };
+const Menu = ({ stock }: Props) => {
+  // const resetStock = () => {
+  //   setStock(premios);
+  //   console.log(stock);
+  //   console.log(premios);
+  // };
 
   return (
     <section className="fade-in menu">
